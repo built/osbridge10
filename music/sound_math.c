32618aeee6670fd2f551e207e68b1689b78ef3e6
Markus Roberts <markus@phage.home>
2010-05-16 15:40:20 -0700

    Added Poor Man's FFT to sound_math
    Also fixed a few minor bugs & cleaned up table creation.

--- music/sound_math.c
+++ music/sound_math.c
@@ -11,12 +11,13 @@ typedef struct {
     double           frequency;
     double           phase_increment;
     double           left_amplitude,right_amplitude;
+    double           s_cross,c_cross;
     volatile double  amplitude_in;
   } wave_form;
 
 typedef struct {
     wave_form tones[MAX_TONES];
-    int length;
+    int       length;
 } tone_table;
 
 static int my_callback(
@@ -34,11 +35,16 @@ static int my_callback(
 
     tone_table* table = (tone_table*)userData;
 
+    for(unsigned int i=0; i < table->length; i++) {
+        table->tones[i].s_cross = 0.0;
+        table->tones[i].c_cross = 0.0;
+    }
+ 
     /* Read input buffer, process data, and fill output buffer. */
     for(unsigned int frame=0; frame<framesPerBuffer; frame++ ) {
         data_in  = *in++;
 
-        /*
+       /*
         TODO: It should compute the cross sections of the data_in with each wave (sin & cos) and then
           update their amplitude_in value after the loop (e.g., in a final loop).  This may require
           temporary storage somewhere?
@@ -46,20 +52,48 @@ static int my_callback(
 
         float  left_total  = 0.0;
         float  right_total = 0.0;
-        float  amp = 0.0;
         for(unsigned int i=0; i < table->length; i++) {
             table->tones[i].phase += table->tones[i].phase_increment;
             if( table->tones[i].phase > Pi ) table->tones[i].phase -= 2*Pi;
-            amp = (float) sin( table->tones[i].phase );
-            left_total  += amp*table->tones[i].left_amplitude;
-            right_total += amp*table->tones[i].right_amplitude;
+            float s_amp = (float) sin( table->tones[i].phase );
+            float c_amp = (float) cos( table->tones[i].phase );
+            left_total  += s_amp*table->tones[i].left_amplitude;
+            right_total += s_amp*table->tones[i].right_amplitude;
+            table->tones[i].s_cross += s_amp*data_in;
+            table->tones[i].c_cross += c_amp*data_in;
         }
         *out++ = left_total;
         *out++ = right_total;
     }
 
+    for(unsigned int i=0; i < table->length; i++) {
+        table->tones[i].amplitude_in = 
+            0.9*table->tones[i].amplitude_in +
+            0.1*(
+              table->tones[i].s_cross*table->tones[i].s_cross+
+              table->tones[i].c_cross*table->tones[i].c_cross
+            );
+    }
+ 
     return paContinue;
 }
+
+void add_tone(tone_table* table,float l_amp,float r_amp,float freq) {
+    int i = table->length;
+    /* TODO: Range check! */
+    table->length += 1;
+    table->tones[i].left_amplitude  = l_amp;
+    table->tones[i].right_amplitude = r_amp;
+    table->tones[i].phase           = 0.0;
+    table->tones[i].phase_increment = freq; 
+       /* 
+       TODO: This is WRONG; the only reason it works is I'm passing the wrong values in as well 
+       TODO: phase_increment should be a constant times the data->xxx.frequency
+       TODO: The constant should be in the user data, and represent
+         2*Pi/Samples_per_second or something like that
+       */
+}
+
 
 #define Stereo  2
 #define Mono    1
@@ -70,23 +104,9 @@ int main(void)
     float const             Sample_rate = 44100.0;
 
     tone_table table;
-
-    table.tones[0].left_amplitude  = 1.0;
-    table.tones[1].right_amplitude = 1.0;
-    table.tones[0].left_amplitude  = 1.0;
-    table.tones[1].right_amplitude = 1.0;
-    table.tones[0].phase      = 0.0;
-    table.tones[1].phase     = 0.0;
-    table.tones[0].phase_increment  = 0.01;
-    table.tones[1].phase_increment = 0.06;
-
-    table.length = 2; // It's a start.
-
-       /*
-       TODO: phase_increment should be a constant times the data->xxx.frequency
-       TODO: The constant should be in the user data, and represent
-         2*Pi/Samples_per_second or something like that
-       */
+    table.length = 0;
+    add_tone(&table,1.0,0.0,0.01);
+    add_tone(&table,0.0,1.0,0.06);
 
     PaStream *stream;
 
