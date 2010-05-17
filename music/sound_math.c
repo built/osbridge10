f3b782c68c565f98801b68616ad343fe91f63b2b
Markus Roberts <markus@phage.home>
2010-05-16 23:08:29 -0700

    Tidying up in sound_math
    Added a real world calibration off my electronic keyboard (it was spot on),
    organized the code a little to make the next step easier, or at least less
    cluttered.

--- music/sound_math.c
+++ music/sound_math.c
@@ -4,11 +4,15 @@
 
 
 #define Pi        (3.14159265)
-#define MAX_TONES (10)
+#define MAX_TONES (20)
 #define Stereo  2
 #define Mono    1
-#define Seconds 1000
-#define Frames_per_buffer 64
+#define Second  *1000
+#define Seconds Second
+#define Millisecond  *1
+#define Milliseconds Millisecond
+/* 0 Frames per buffer means use the optimal # */
+#define Frames_per_buffer 0
 #define Sample_rate 44100.0
 
 typedef struct {
@@ -82,49 +86,136 @@ void add_tone(tone_table* table,float l_amp,float r_amp,float freq) {
     table->tone[i].phase_increment = freq*2*Pi/Sample_rate;
 }
 
+#define Half_step 1.0594630943593
+#define Full_step (Half_step*Half_step)
+#define A_1       220.0
+#define B_1       (A_1*Full_step)
+#define C_2       (B_1*Half_step)
+#define D_2       (C_2*Full_step)
+#define E_2       (D_2*Full_step)
+#define F_2       (E_2*Half_step)
+#define G_2       (F_2*Full_step)
+#define A_2       (A_1*2)
+#define B_2       (B_1*2)
 
-int main(void)
-{
-    float half_step = 1.0594630943593;
-    float full_step = half_step*half_step;
-    float A_1 = 440.0;
-    float B_1 = A_1*full_step;
-    float C_2 = B_1*half_step;
-    float D_2 = C_2*full_step;
-    float E_2 = D_2*full_step;
-    float F_2 = E_2*half_step;
-    tone_table table;
-    table.tones = 0;
-    table.samples = 0;
-    add_tone(&table,0.0,0.0,A_1);
-    add_tone(&table,0.0,0.0,B_1);
-    add_tone(&table,1.0,0.0,C_2);
-    add_tone(&table,0.0,0.0,D_2);
-    add_tone(&table,0.0,0.5,E_2);
-    add_tone(&table,0.0,0.0,F_2);
-
-    PaStream *stream;
+PaStream* start(tone_table* table) {
+    PaStream* result;
 
     Pa_Initialize();
 
     Pa_OpenDefaultStream(
-        &stream, Mono/*in*/, Stereo/*out*/, paFloat32, Sample_rate, Frames_per_buffer,
-        my_callback, &table
+        &result, Mono/*in*/, Stereo/*out*/, paFloat32, Sample_rate, Frames_per_buffer,
+        my_callback, table
         );
-    Pa_StartStream( stream );
-    Pa_Sleep( 10*Seconds );
+    Pa_StartStream( result );
+    return result;
+}
+
+void stop(PaStream* stream) {
     Pa_StopStream( stream );
     Pa_CloseStream( stream );
+    Pa_Terminate();
+}
 
-    for(int i=0;i<table.tones;i++) {
+void report_what_we_heard(tone_table* table) {
+    for(int i=0;i<table->tones;i++) {
         printf("%8.3f (%5.3f) was at %10.5f\n",
-           table.tone[i].frequency,
-           table.tone[i].phase_increment,
-           table.tone[i].amplitude_in/100.0
+           table->tone[i].frequency,
+           table->tone[i].phase_increment,
+           table->tone[i].amplitude_in/100.0
            );
     }
+}
 
-    Pa_Terminate();
+void calibrate()
+{
+    tone_table table;
+    table.tones = 0;
+    table.samples = 0;
+
+    add_tone(&table,0.0,0.0,A_1*0.996);
+    add_tone(&table,0.0,0.0,A_1*0.997);
+    add_tone(&table,0.0,0.0,A_1*0.998);
+    add_tone(&table,0.0,0.0,A_1*0.999);
+    add_tone(&table,0.0,0.0,A_1);
+    add_tone(&table,0.0,0.0,A_1*1.001);
+    add_tone(&table,0.0,0.0,A_1*1.002);
+    add_tone(&table,0.0,0.0,A_1*1.003);
+    add_tone(&table,0.0,0.0,A_1*1.004);
+
+    printf("Play you A-below-middle-C for the next ten seconds\n");
+    PaStream* stream = start(&table);
+    Pa_Sleep( 10 Seconds );
+    stop(&stream);
+    report_what_we_heard(&table);
+}
+
+void slide_test()
+{
+    tone_table table;
+    table.tones = 0;
+    table.samples = 0;
+
+    add_tone(&table,1.0,0.0,A_1);
+    add_tone(&table,0.0,0.0,B_1);
+    add_tone(&table,0.0,0.0,C_2);
+    add_tone(&table,0.0,0.0,D_2);
+    add_tone(&table,0.0,0.0,E_2);
+    add_tone(&table,0.0,1.0,F_2);
+    add_tone(&table,0.0,0.0,F_2-A_1);
+
+    PaStream* stream = start(&table);
+
+    for(int i=0;i < 100;i++) {
+        table.tone[2].left_amplitude = i*0.01;
+        table.tone[5].right_amplitude = (100-i)*0.01;
+        Pa_Sleep( 20 Milliseconds );
+    }
+
+    Pa_Sleep( 1 Second );
+
+    for(int i=0;i < 100;i++) {
+        table.tone[2].left_amplitude = (100-i)*0.01;
+        table.tone[5].right_amplitude = i*0.01;
+        Pa_Sleep( 20 Milliseconds );
+    }
+
+    stop(&stream);
+    report_what_we_heard(&table);
+}
+
+void chord_test()
+{
+    tone_table table;
+    table.tones = 0;
+    table.samples = 0;
+
+    add_tone(&table,2.0,0.0,A_1);
+    add_tone(&table,0.5,0.5,C_2);
+    add_tone(&table,0.0,1.0,F_2);
+    add_tone(&table,0.0,0.0,A_2);
+
+    PaStream* stream = start(&table);
+    Pa_Sleep( 5 Seconds );
+    table.tone[0].left_amplitude = 0.0;
+    table.tone[3].left_amplitude = 1.0;
+    Pa_Sleep( 5 Seconds );
+    stop(&stream);
+    report_what_we_heard(&table);
+}
+
+
+int main(void)
+{
+
+    /* 
+    TODO: command line switching between modes or...?
+    */
+
+    chord_test();
+    /* calibrate(); */
+    /* slide_test(); */
+
     return 0;
 }
 
